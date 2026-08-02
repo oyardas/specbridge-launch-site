3b99f79b1489e2e8f1dca77f83ff66ef417cc344
OnderYardas <92328486+oyardas@users.noreply.github.com>
2026-08-02 23:29:38 +0300

    Compress encrypted KAYAS portal payload

--- kayas/assets/payload-init.js
+++ kayas/assets/payload-init.js
@@ -1,1 +1,1 @@
-window.KAYAS_SEC={salt:"z6+gI4dfDkQUIZkzFdGEcg==",iv:"0IHZRQ0m2/IGH0Li",iterations:310000,parts:[]};
+window.KAYAS_SEC={salt:"QBcPWbKjRcu+R8jEjX+1Ig==",iv:"jO8Kz1+SdoldAclO",iterations:310000,gzip:true,parts:[]};
