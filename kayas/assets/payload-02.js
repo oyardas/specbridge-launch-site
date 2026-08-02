42b2346c454a5dae936065e0f833fe8881998890
OnderYardas <92328486+oyardas@users.noreply.github.com>
2026-08-02 23:35:48 +0300

    Switch KAYAS portal to compact encrypted payload

---
+++ kayas/assets/payload-02.js
@@ -0,0 +1,2 @@
+window.KAYAS_SEC.parts=[];
+document.write('<script src="assets/payload-c00.js?v=20260802-4"><\/script><script src="assets/payload-c01.js?v=20260802-4"><\/script><script src="assets/payload-c02.js?v=20260802-4"><\/script><script src="assets/payload-c03.js?v=20260802-4"><\/script>');
